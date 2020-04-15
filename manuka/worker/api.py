86e39b1ce5cfeadd8ef042a603283e457aee298f
Sam Morrison <sorrison@gmail.com>
2020-04-16 09:26:59 +1000

    Inital copy of code from rcshibboleth decanted
    Change-Id: I6b4027c265ba31e47c0f1686e87a42edfc86dd2b

---
+++ manuka/worker/api.py
@@ -0,0 +1,27 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import oslo_messaging
+
+from manuka.common import rpc
+
+
+class WorkerAPI(object):
+
+    def __init__(self):
+        target = oslo_messaging.Target(topic='manuka-worker', version='1.0')
+        self._client = oslo_messaging.RPCClient(rpc.TRANSPORT, target)
+
+    def create_user(self, ctxt, shib_user):
+        cctxt = self._client.prepare(version='1.0')
+        cctxt.cast(ctxt, 'create_user', shib_user=shib_user)
