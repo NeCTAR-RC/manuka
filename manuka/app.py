57ccc77ad8585f3a7d7267fa9a3e1176a3974e01
Sam Morrison <sorrison@gmail.com>
2020-04-21 16:22:06 +1000

    Add a keystone protected rest api for user operations
    Change-Id: Ic217d66aea2f21bbc18d953cd8d4586e8c93bbee

--- manuka/app.py
+++ manuka/app.py
@@ -16,24 +16,29 @@ import os
 from beaker import middleware as beaker
 from flask import Flask
 from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_middleware import healthcheck
 
+from manuka.api import v1 as api_v1
+from manuka.common import keystone
 from manuka.common import rpc
 from manuka import config
-from manuka.extensions import db
-from manuka.extensions import migrate
+from manuka import extensions
 from manuka import shib_faker
 from manuka import views
 
 
 CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
 
 
-def create_app(test_config=None, conf_file=None):
+def create_app(test_config=None, conf_file=None, init_config=True):
     # create and configure the app
-    if conf_file:
-        config.init(conf_file=conf_file)
-    else:
-        config.init()
+    if init_config:
+        if conf_file:
+            config.init(conf_file=conf_file)
+        else:
+            config.init()
     app = Flask(__name__)
     if test_config is None:
         app.config.from_mapping(
@@ -47,6 +52,7 @@ def create_app(test_config=None, conf_file=None):
 
     config.setup_logging(CONF)
     register_extensions(app)
+    register_resources(extensions.api)
     register_blueprints(app)
     # ensure the instance folder exists
     try:
@@ -58,16 +64,28 @@ def create_app(test_config=None, conf_file=None):
         app.wsgi_app = shib_faker.FakeShibboleth(app.wsgi_app)
         app.wsgi_app = beaker.SessionMiddleware(app.wsgi_app)
 
+    app.wsgi_app = healthcheck.Healthcheck(app.wsgi_app)
+
+    if CONF.auth_strategy == 'keystone':
+        app.wsgi_app = keystone.KeystoneContext(app.wsgi_app)
+        app.wsgi_app = keystone.SkippingAuthProtocol(app.wsgi_app, {})
     rpc.init()
+
     return app
 
 
 def register_extensions(app):
     """Register Flask extensions."""
-    db.init_app(app)
-    migrate.init_app(app, db)
+    extensions.api.init_app(app)
+    extensions.db.init_app(app)
+    extensions.migrate.init_app(app, extensions.db)
+    extensions.ma.init_app(app)
 
 
 def register_blueprints(app):
     app.register_blueprint(views.default_bp)
     app.register_blueprint(views.login_bp)
+
+
+def register_resources(api):
+    api_v1.initialize_resources(api)
