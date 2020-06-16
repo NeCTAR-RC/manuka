1dc37eb246064c311e5112b5e2bbf05545109053
Sam Morrison <sorrison@gmail.com>
2020-06-16 13:15:56 +1000

    Use pbr to parse requirements
    Change-Id: I3e56406872b56bb3ac8ea0480295900e54293e45

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
-from setuptools import find_packages, setup
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+import setuptools
 
-requirements = parse_requirements("requirements.txt", session=False)
+from pbr.packaging import parse_requirements
 
 
-setup(
+setuptools.setup(
     name='manuka',
     version='0.1.0',
     description=('User management for the Nectar Research Cloud'),
     author='Sam Morrison',
     author_email='sorrison@gmail.com',
     url='https://github.com/NeCTAR-RC/manuka',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
             'manuka-worker = manuka.cmd.worker:main',
@@ -33,7 +41,8 @@ setup(
         ],
     },
     include_package_data=True,
-    install_requires=[str(r.req) for r in requirements],
+    setup_requires=['pbr>=3.0.0'],
+    install_requires=parse_requirements(),
     license="Apache",
     zip_safe=False,
 )
