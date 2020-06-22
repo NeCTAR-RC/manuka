009fa7a03e7bdf80a7adb829ed142ebff48207ca
Stephen Crawley <s.crawley@uq.edu.au>
2020-06-22 15:01:17 +1000

    Lookup missing orcid on login
    Check if the user has an orcid recorded.  If not, start a worker
    job to do an email -> orcid lookup from the configured orcid service.
    If that succeeds, the user's orcid will be set.
    
    Change-Id: I5c2a93f6e2e4b392ae681e317700385dfeb66506

--- manuka/views.py
+++ manuka/views.py
@@ -207,6 +207,11 @@ def root():
         data = {"user": user}
         return flask.render_template("username_form.html", **data)
 
+    if not db_user.orcid:
+        worker = worker_api.WorkerAPI()
+        ctxt = context.RequestContext()
+        worker.refresh_orcid(ctxt, db_user.id)
+
     # sjjf: default to the configured target URL, but allow the source
     # to specify a different return-path. The specified return path is
     # then verfied against a white list.
--- manuka/worker/api.py
+++ manuka/worker/api.py
@@ -16,7 +16,7 @@ import oslo_messaging
 from manuka.common import rpc
 
 # Current API version
-API_VERSION = '1.0'
+API_VERSION = '1.1'
 
 
 class WorkerAPI(object):
@@ -25,6 +25,7 @@ class WorkerAPI(object):
     Version history:
 
     1.0 - Add create_user
+    1.1 - Add refresh_orcid
     """
 
     def __init__(self):
@@ -35,3 +36,7 @@ class WorkerAPI(object):
     def create_user(self, ctxt, attrs):
         cctxt = self._client.prepare(version='1.0')
         cctxt.cast(ctxt, 'create_user', attrs=attrs)
+
+    def refresh_orcid(self, ctxt, user_id):
+        cctxt = self._client.prepare(version='1.1')
+        cctxt.cast(ctxt, 'refresh_orcid', user_id=user_id)
